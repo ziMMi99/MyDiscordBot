38997c44ee2e8b3c483826afc3deffda25b13448
ziMMi99 <100197291+ziMMi99@users.noreply.github.com>
2023-12-22 23:56:34 +0100

    Delete src/main/java/org/example/secret.java

--- src/main/java/org/example/secret.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public class secret {
-    public static String token = "MTE4NTU4MTY0MjcwNjg1ODAyNQ.GAU0b5.HcOoHIX1TF-jclDh1Nh3u1om1HGs40aIJ-aWzI";
-    public static long guildID = 491667562250240011L;
-}
